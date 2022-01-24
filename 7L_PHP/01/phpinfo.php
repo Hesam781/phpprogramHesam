d580aa8c3ecfc029f078e33139dec51fbe0cbf13
Hesam781 <64691956+Hesam781@users.noreply.github.com>
2022-01-24 10:47:05 -0600

    7L_PHP

---
+++ 7L_PHP/01/phpinfo.php
@@ -0,0 +1,60 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<!--p#ali>ul>li*5-->
+<p id="ali">
+<ul>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+</ul>
+</p>
+<!--table.tbl1>tr*5>td*2-->
+<table class="tbl1" style="padding: 8px;">
+    <tr>
+        <td></td>
+        <td></td>
+    </tr>
+    <tr>
+        <td></td>
+        <td></td>
+    </tr>
+    <tr>
+        <td></td>
+        <td></td>
+    </tr>
+    <tr>
+        <td></td>
+        <td></td>
+    </tr>
+    <tr>
+        <td></td>
+        <td></td>
+    </tr>
+</table>
+</body>
+</html>
+<?php
+//phpinfo();
+//ctrl+d  copy line
+echo 'Hi';
+echo 'Hi';
+echo 'Hi';
+echo 'Hi';
+echo 'Hi';
+echo 'Hi';
+//ctrl+alt+l    select command and
+if (true){echo 'true';}
+if (true) {
+    echo 'true';
+}
+echo '';
